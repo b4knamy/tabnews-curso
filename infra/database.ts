9948e85ec989036057aea07fa8d447f05b4e2fbe
Rogério França <rogeriofranca.contato@gmail.com>
2025-04-26 17:29:56 -0300

    infra: database file ´database.ts´

---
+++ infra/database.ts
@@ -0,0 +1,27 @@
+import { Client, QueryConfig } from "pg";
+
+const clientConfig = {
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const query = async (queryObject: string | QueryConfig) => {
+  const client = new Client(clientConfig);
+  await client.connect();
+
+  try {
+    const query = await client.query(queryObject);
+    return query;
+  } catch (err) {
+    console.log(`\nDATABASE ERROR: ${err}\n`);
+  } finally {
+    await client.end();
+  }
+};
+
+export default {
+  query,
+};
