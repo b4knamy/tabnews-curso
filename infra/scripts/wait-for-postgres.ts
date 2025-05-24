320917f55573c0027194880d8f6d1e6534951c4a
Rogério França <rogeriofranca.contato@gmail.com>
2025-05-24 17:36:28 -0300

    update: db container name to `postgres-dev`

---
+++ infra/scripts/wait-for-postgres.ts
@@ -0,0 +1,22 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  const handleReturn = (error, stdout) => {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log(
+      "\n\n====== - Postgres is ready to accept connections. - =======\n",
+    );
+  };
+
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+}
+
+process.stdout.write(
+  "\n====== - Waiting Postgres to accept connections - =======\n",
+);
+checkPostgres();
